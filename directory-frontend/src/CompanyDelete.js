9b259bdc76072ff7f55dfe3e7b16570f1683050d
gtblackmore <gtblackmore@gmail.com>
2021-02-08 08:36:12 -0500

    stub out company components

---
+++ directory-frontend/src/CompanyDelete.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CompanyDelete = () => {
+  return <div>CompanyDelete</div>
+};
+
+export default CompanyDelete;
---
+++ directory-frontend/src/CompanyIndex.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CompanyIndex = () => {
+  return <div>CompanyIndex</div>
+};
+
+export default CompanyIndex;
