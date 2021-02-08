12faeaf8cf0f4bd306ec66aae851a7ad2a4d1298
gtblackmore <gtblackmore@gmail.com>
2021-02-08 00:17:54 -0500

    add migrations for company

--- directory_api/db/migrate/20210208050354_create_companies.rb
+++ directory_api/db/migrate/20210208050354_create_companies.rb
@@ -1,6 +1,15 @@
 class CreateCompanies < ActiveRecord::Migration[6.0]
   def change
     create_table :companies do |t|
+      t.string :name
+      t.string :address1
+      t.string :address2
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :phone
+      t.string :email
+
 
       t.timestamps
     end
