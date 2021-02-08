8a58a720d4a45cf8c651ae327eee4f4e4ceca76e
gtblackmore <gtblackmore@gmail.com>
2021-02-08 08:37:39 -0500

    add router and navbar to app.js

--- directory-frontend/src/App.js
+++ directory-frontend/src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import NavBar from './NavBar'
+import CompanyCreate from './CompanyCreate'
+import CompanyDelete from './CompanyDelete'
+import CompanyDetail from './CompanyDetail'
+import CompanyEdit from './CompanyEdit'
+import CompanyIndex from './CompanyIndex'
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <Router>
+      <NavBar />
+        <div>
+          <Route path="/" exact component={CompanyIndex} />
+          <Route path="/companies/edit" component={CompanyEdit} />
+          <Route path="/companies/show"  component={CompanyDetail} />
+          <Route path="/companies/delete"  component={CompanyDelete} />
+          <Route path="/companies/new"  component={CompanyCreate} />
+        </div>
+    </Router>
+
+  )
 };
 
 export default App;
