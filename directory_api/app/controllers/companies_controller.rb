0fc7cc8c364170f5e6daca49a2c49c8d2875a278
gtblackmore <gtblackmore@gmail.com>
2021-02-25 09:19:31 -0500

    add strong params to api backend

--- directory_api/app/controllers/companies_controller.rb
+++ directory_api/app/controllers/companies_controller.rb
@@ -46,6 +46,6 @@ class CompaniesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def company_params
-      params.fetch(:company, {})
+      params.require(:company).permit(:name, :address1, :address2, :city, :state, :zip, :phone, :email)
     end
 end
