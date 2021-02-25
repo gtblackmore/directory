4ff3872d62096882067098bb88f5d911f6b906ce
gtblackmore <gtblackmore@gmail.com>
2021-02-25 09:17:56 -0500

    create company item component

---
+++ directory-frontend/src/CompanyItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CompanyItem = ({company, onCompanySelect}) => {
+  return (
+    <div className="ui items">
+
+    <div className="item">
+      <div className="ui small image">
+        <img src="https://via.placeholder.com/150" alt={company.name} />
+      </div>
+    <div className="content">
+      <div className="header">{company.name}</div>
+      <div className="meta">
+        <span className="address1">{company.address1}</span>
+        <br />
+        <span className="address2">{company.address2}</span>
+        <br />
+        <span className="city-info">{company.city}, {company.state} {company.zip}</span>
+      </div>
+      <br />
+      <Link to={`/companies/edit/${company.id}`} onClick={() => onCompanySelect(company)} className="ui button">Edit Company</Link>
+      </div>
+    </div>
+    </div>
+  )};
+
+export default CompanyItem;
