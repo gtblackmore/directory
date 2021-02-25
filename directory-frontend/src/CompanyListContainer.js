fbd972179f8742ec8e61ddd90e32d485e1205a0f
gtblackmore <gtblackmore@gmail.com>
2021-02-25 09:17:10 -0500

    create company component

---
+++ directory-frontend/src/CompanyListContainer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchCompanies } from './actionCreator';
+import CompanyList from './CompanyList';
+
+class CompanyListContainer extends React.Component {
+
+  componentDidMount(){
+   this.props.fetchCompanies();
+  };
+
+  onCompanySelect = (company) => {
+    this.setState({selectedCompany: company})
+
+  }
+
+  render() {
+    if(!this.props.companies) {
+      return <div>Loading...</div>
+    }
+    
+    return (
+      <div>
+        <CompanyList onCompanySelect={this.onCompanySelect} companies={this.props.companies}/>
+      </div>
+      )
+    }
+}
+
+const mapStateToProps = (state) => {
+  return { companies: state.companies }
+};
+
+export default connect(mapStateToProps, { fetchCompanies })(CompanyListContainer);
