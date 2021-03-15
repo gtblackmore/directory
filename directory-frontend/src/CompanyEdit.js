da4761e2da40b4b5f3ef2c6ec6387c3745d7595d
gtblackmore <gtblackmore@gmail.com>
2021-03-15 06:21:34 -0400

    refactor create and edit forms into one form

--- directory-frontend/src/CompanyEdit.js
+++ directory-frontend/src/CompanyEdit.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchCompany, editCompany } from './actionCreator';
+import CompanyForm from './CompanyForm';
 
-const CompanyEdit = () => {
-  return <div>CompanyEdit</div>
-};
+class CompanyEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchCompany(this.props.match.params.id);
+  }
 
-export default CompanyEdit;
+  onSubmit = (formValues) => {
+    this.props.editCompany(this.props.match.params.id, formValues)
+  }
+  
+  render() {
+    if (!this.props.company > 1) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+      Edit Company
+      <CompanyForm
+        initialValues={this.props.company}
+        onSubmit={this.onSubmit}
+      />
+      </div>
+    )}
+}
+const mapStateToProps = (state, ownProps) => {
+  console.log(state)
+  return {company: state.companies.selectedCompany}
+}
+
+export default connect(mapStateToProps, { fetchCompany, editCompany })(CompanyEdit);
